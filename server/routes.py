8d109bea0be0df2bc9dde00cb9e6258405fd4fba
Conner DeFeo <ninjanerozz@gmail.com>
2026-06-30 17:34:09 -0400

    stealing the basic layout of peakflow ai agnet

---
+++ server/routes.py
@@ -0,0 +1,29 @@
+import logging
+from fastapi import APIRouter, WebSocket, Response
+from twilio.twiml.voice_response import VoiceResponse, Connect, ConversationRelay
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+# Personal routes
+@router.post(f"/handshake")
+def ws_handshake():
+    response = VoiceResponse()
+    connect = Connect()
+    conversationrelay = ConversationRelay(
+        url="MOCK URL",
+        welcome_greeting="MOCK GREETING",
+        welcome_greeting_interruptible = False
+    )
+    conversationrelay.language(
+        code="en-US",
+        tts_provider="ElevenLabs",
+        voice="gfRt6Z3Z8aTbpLfexQ7N",
+    )
+    connect.append(conversationrelay)
+    response.append(connect)
+    return Response(content=str(response), media_type="application/xml")
+
+@router.websocket(f"/")
+async def websocket_handler(websocket: WebSocket):
+    return
