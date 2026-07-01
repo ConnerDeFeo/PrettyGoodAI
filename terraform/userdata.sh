0ec227ec8aac57230311066eebf70ff63f044ee7
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-01 11:55:13 -0400

    user data working

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -10,10 +10,10 @@ sudo dnf install -y python3 python3-pip git
 # Clone your repo
 cd /home/ec2-user
 git clone https://github.com/ConnerDeFeo/PrettyGoodAI.git
-cd PrettyGoodAI/server
+cd PrettyGoodAI
 
 # Install Python dependencies
 pip3 install -r requirements.txt
 
 # Fix ownership since user_data runs as root
-chown -R ec2-user:ec2-user /home/ec2-user/PrettyGoodAI/server
+chown -R ec2-user:ec2-user /home/ec2-user/PrettyGoodAI
