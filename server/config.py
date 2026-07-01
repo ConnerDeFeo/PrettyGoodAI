acd48b6cdbd6f3e408f4d4ba11fa9607f4cd6ab8
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-01 11:30:02 -0400

    lets goooo

--- server/config.py
+++ server/config.py
@@ -6,5 +6,6 @@ bedrock = boto3.client("bedrock-runtime", region_name="us-east-2")
 
 SERVER_DOMAIN = ""
 CONVERSATION_MODEL = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
-SYSTEM_PROMPT = ""
+SYSTEM_PROMPT = "You are a nice AI agent having a freindly conversation over the phone." \
+"Keep your responses short and concise like you are on the phone, use basic mannerisms as well."
 MAX_OUTPUT_TOKENS = 300
