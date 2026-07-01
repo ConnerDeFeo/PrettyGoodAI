2e9ea0be3ae3f61a36c8f4b9d35714db352a3e89
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-01 16:18:45 -0400

    adding the actuall server domain to the config

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
 conversation_history = dynamodb.Table("pgai_conversation_history")
 bedrock = boto3.client("bedrock-runtime", region_name="us-east-2")
 
-SERVER_DOMAIN = ""
+SERVER_DOMAIN = "pgai.connerdefeo.com"
 CONVERSATION_MODEL = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
 SYSTEM_PROMPT = "You are a nice AI agent having a freindly conversation over the phone." \
 "Keep your responses short and concise like you are on the phone, use basic mannerisms as well."
