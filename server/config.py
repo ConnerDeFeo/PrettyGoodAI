63a7de7d831a0c0985d7f4fb22733f443b7e89c8
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-01 10:14:46 -0400

    basic scaffolding for now

--- server/config.py
+++ server/config.py
@@ -2,4 +2,8 @@ import boto3
 
 dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
 conversation_history = dynamodb.Table("pretty_good_ai_conversation_history")
-call_information = dynamodb.Table("pretty_good_ai_call_information")
+bedrock = boto3.client("bedrock-runtime", region_name="us-east-2")
+
+CONVERSATION_MODEL = "Claude-haiku"
+SYSTEM_PROMPT = ""
+MAX_OUTPUT_TOKENS = 400
--- server/dynamo.py
+++ server/dynamo.py
@@ -7,6 +7,5 @@ logger = logging.getLogger(__name__)
 def get_call_data(self, phone_number: str, default: dict) -> dict:
     return
 
-
 def save_call_data(self, phone_number: str, data: dict):
     return
