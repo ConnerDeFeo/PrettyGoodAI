cb10bd6f16363b74bbadee74c1a0ed550993ddd0
Conner DeFeo <ninjanerozz@gmail.com>
2026-07-01 11:22:32 -0400

    using claude

--- server/config.py
+++ server/config.py
@@ -5,6 +5,6 @@ conversation_history = dynamodb.Table("pgai_conversation_history")
 bedrock = boto3.client("bedrock-runtime", region_name="us-east-2")
 
 SERVER_DOMAIN = ""
-CONVERSATION_MODEL = "Claude-haiku"
+CONVERSATION_MODEL = "us.anthropic.claude-haiku-4-5-20251001-v1:0"
 SYSTEM_PROMPT = ""
 MAX_OUTPUT_TOKENS = 400
